4a77985772828a6eca49f8e62e7775a34b6ab4e6
Angarag Gansukh <angaraggansukh@csu.fullerton.edu>
2023-11-26 14:10:54 -0800

    Create chat function

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import AudioRecorder from './components/AudioRecorder';
 import { Button } from 'react-bootstrap';
 import React, { useState } from 'react';
 import axios from 'axios';
+import OpenAI from 'openai';
 
 
 
 function App() {
 
+  const openai = new OpenAI();
   const [audioFile, setAudioFile] = useState(null);
   const [audioText, setAudioText] = useState("");
 
@@ -30,6 +32,16 @@ function App() {
     })
   };
 
+  const uploadPromptToChat = async () => {
+    openai.chat.completions.create({
+      messages: [{"role": "user", "content": "What is 2+2?"}],
+      model: "gpt-3.5-turbo"
+    })
+    .then(res => {
+      console.log(res.data)
+    })
+  };
+
   return (
     <div className="App">
       <section>
@@ -46,7 +58,7 @@ function App() {
           />
           { audioFile !== null && (
             <div className='upload-file-button'>
-              <Button onClick={uploadFileToWhisper}>Upload Audio File</Button>
+              <Button onClick={uploadPromptToChat}>Upload Audio File</Button>
             </div>
           )}
       </div>
